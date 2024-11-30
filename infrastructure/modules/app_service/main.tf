964f578ebd46d8cad064ed66d76add89341bc05f
Guayben <clement.calujek@gmail.com>
2024-11-30 22:49:41 +0100

    fix : docker registry url

--- infrastructure/modules/app_service/main.tf
+++ infrastructure/modules/app_service/main.tf
@@ -17,7 +17,7 @@ resource "azurerm_linux_web_app" "shop_app_service" {
   site_config {
     always_on = true
     application_stack {
-      docker_registry_url      = "guayben/shop-app:latest"
+      docker_registry_url      = "https://index.docker.io/v1/"
       docker_registry_username = var.docker_registry_username
       docker_registry_password = var.docker_registry_password
       docker_image_name        = var.docker_image_name
