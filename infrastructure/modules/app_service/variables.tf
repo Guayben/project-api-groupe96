3670dffeb394bef0c733b7908d9295a9b80937e0
GuillaumeCazier <113700910+GuillaumeCazier@users.noreply.github.com>
2024-12-02 10:12:38 +0100

    Comment variables.tf

--- infrastructure/modules/app_service/variables.tf
+++ infrastructure/modules/app_service/variables.tf
@@ -1,56 +1,69 @@
+# Nom du service plan Azure utilisé pour héberger les applications.
 variable "service_plan_name" {
-  type = string
+  type = string 
 }
 
+# Nom de l'application web à déployer.
 variable "app_service_name" {
-  type = string
+  type = string 
 }
 
+# Région Azure où les ressources seront déployées.
 variable "location" {
-  type = string
+  type = string 
 }
 
+# Groupe de ressources Azure dans lequel les ressources seront créées.
 variable "resource_group_name" {
-  type = string
+  type = string 
 }
 
+# Identifiant utilisateur pour le registre Docker.
 variable "docker_registry_username" {
-  type = string
+  type = string 
 }
 
+# Mot de passe pour l'authentification au registre Docker.
 variable "docker_registry_password" {
-  type = string
+  type = string 
 }
 
+# Nom de l'image Docker à utiliser pour l'application.
 variable "docker_image_name" {
-  type = string
+  type = string 
 }
 
+# ID du sous-réseau dans lequel l'application sera connectée.
 variable "app_service_subnet_id" {
-  type = string
+  type = string 
 }
 
+# Clé API utilisée par l'application.
 variable "api_key" {
-  type = string
+  type = string 
 }
 
+# Endpoint de la base de données CosmosDB.
 variable "cosmosdb_endpoint" {
-  description = "Endpoint of the Cosmos DB"
-  type        = string
+  description = "Endpoint of the Cosmos DB" 
+  type        = string                     
 }
 
+# Clé de lecture seule pour accéder à CosmosDB.
 variable "cosmosdb_readonly_key" {
-  description = "Read-only key for the Cosmos DB"
-  type        = string
-  sensitive   = true
+  description = "Read-only key for the Cosmos DB" 
+  type        = string                           
+  sensitive   = true                              # Marque la variable comme sensible pour éviter qu'elle ne soit exposée.
 }
 
+# Nom de la base de données CosmosDB.
 variable "cosmosdb_database_name" {
-  description = "Name of the Cosmos DB database"
-  type        = string
+  description = "Name of the Cosmos DB database" 
+  type        = string                           
 }
 
+# Nom du conteneur dans la base de données CosmosDB.
 variable "cosmosdb_container_name" {
-  description = "Name of the Cosmos DB container"
-  type        = string
+  description = "Name of the Cosmos DB container" 
+  type        = string                            
 }
